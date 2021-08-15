0ca9f58e92db5a71a52b4dd71d9ad7fc4a018322
WahajRashid1020 <wahaj1020@gmail.com>
2021-08-15 21:51:15 +0500

    final

---
+++ lib/info_page.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+
+import 'package:awesomeapp/core/store.dart';
+import 'package:awesomeapp/models/cart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share/share.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:velocity_x/velocity_x.dart';
+
+// ignore: camel_case_types
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[350],
+        title: "Place Your Order On WhatsApp"
+            .text
+            .bold
+            .color(Colors.indigo[900])
+            .make(),
+      ),
+      body: Column(
+        children: [
+          Cartlist().p32().expand(),
+          Divider(),
+          Mytotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class Cartlist extends StatelessWidget {
+  final CartModel _cart = (VxState.store as MyStore).cart;
+
+  @override
+  Widget build(BuildContext context) {
+    VxState.listen(context, to: [RemoveMutation]);
+    return SafeArea(
+      child: Container(
+        // ignore: null_aware_in_condition
+        child: _cart.items?.isEmpty
+            ? "Notihng To Show".text.xl2.color(Colors.white).makeCentered()
+            : ListView.builder(
+                shrinkWrap: true,
+                itemCount: _cart.items?.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    tileColor: Colors.black12,
+                    title: _cart.items[index].name.text
+                        .color(Colors.white)
+                        .bold
+                        .make(),
+                  );
+                }),
+      ),
+    );
+  }
+}
+
+class Mytotal extends StatelessWidget {
+  final CartModel _cart = (VxState.store as MyStore).cart;
+
+  @override
+  Widget build(BuildContext context) {
+    num size = _cart.items.length;
+    var arr = [size];
+    List<String> list = [];
+    return SizedBox(
+      height: 140,
+      child: Column(
+        children: [
+          "Note".text.center.color(Colors.red).make(),
+          "Place your order on our WhatsApp number which is given below"
+              .text
+              .color(Colors.green)
+              .make()
+              .p16(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Center(
+                child: SelectableText(
+                  "03433093451",
+                  style: TextStyle(
+                      color: Colors.lightGreenAccent[400],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                  textAlign: TextAlign.left,
+                  onTap: () => print('Tapped'),
+                  toolbarOptions: ToolbarOptions(
+                    copy: true,
+                    selectAll: true,
+                  ),
+                  showCursor: true,
+                  cursorWidth: 2,
+                  cursorColor: Colors.red,
+                  cursorRadius: Radius.circular(5),
+                ),
+              ).p12(),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  const oneSec = Duration(seconds: 200);
+                  // Timer.periodic(oneSec, (Timer t) =>).cancel();
+
+                  dialog(context);
+                  opp(size, list, _cart);
+                  // ;
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+                child: "Place Order".text.make(),
+              ).w32(context)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void opp(size, list, _cart) {
+  for (int i = 0; i <= size - 1; i++) {
+    list.add("\n" + "${i + 1}-" + _cart.items[i].name);
+
+    // arr.add("${_cart.items[i].name}");
+
+  }
+  list.insert(size, "\n" + "Total Price \$${_cart.totalPrice}");
+
+  Share.share(list.toString());
+}
+
+void dialog(context) {
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: const Text('Note'),
+      content: const Text(
+          'Place your order on our WhatsApp number which is given below'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'Cancel'),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'OK'),
+          child: const Text('OK'),
+        ),
+      ],
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,302 @@
+import 'dart:convert';
+
+import 'package:awesomeapp/add_to_cart.dart';
+import 'package:awesomeapp/cart_page.dart';
+import 'package:awesomeapp/core/store.dart';
+import 'package:awesomeapp/detail.dart';
+import 'package:awesomeapp/info_page.dart';
+import 'package:awesomeapp/login-page.dart';
+import 'package:awesomeapp/models/cart.dart';
+import 'package:awesomeapp/models/catalog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// ignore: unused_import
+import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
+// ignore: unused_import
+import 'item_widget.dart';
+
+void main() {
+  runApp(
+    VxState(
+      store: MyStore(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Flutter App",
+          home: HomePage(),
+          theme: ThemeData(
+              primarySwatch: Colors.indigo,
+              primaryTextTheme: GoogleFonts.latoTextTheme()),
+          routes: {
+            "/login": (context) => MyLoginPage(),
+            "/home": (context) => HomePage(),
+            "/cart": (context) => Cart_page(),
+            "/info": (context) => InfoPage(),
+            "/details": (context) => DetailsPage(
+                  catalog: null,
+                ),
+          }),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // ignore: unused_field
+  // ignore: unused_field
+  TextEditingController _nameController = TextEditingController();
+  var mytext = "Change It";
+  var data;
+  // final url = 'http://jsonplaceholder.typicode.com/photos';
+  final url = 'https://qasim-electronics.herokuapp.com/api/products/';
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+    //  getData();
+  }
+
+  loadData() async {
+    // var catjson = await rootBundle.loadString("assets/files/catalog.json");
+    // final catdata = jsonDecode(catjson);
+    // var pd = catdata["products"];
+    // CatalogModel.items =
+    //     List.from(pd).map<Item>((item) => Item.fromMap(item)).toList();
+    // setState(() {});
+
+    var catjson = await http.get(Uri.parse(url));
+    final catjs = catjson.body;
+    // await rootBundle.loadString("assets/files/catalog.json");
+    final catdata = jsonDecode(catjs);
+    // var pd = catdata["products"];
+    // print("Products"+pd);
+    CatalogModel.items =
+        List.from(catdata).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  // getData() async {
+  //   var res = await http.get(Uri.parse(url));
+  //   data = jsonDecode(res.body);
+  //   print(data);
+  // }
+
+  Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
+    return Scaffold(
+        backgroundColor: Colors.grey[350],
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (ctx, _) => FloatingActionButton(
+            backgroundColor: Colors.yellow[800],
+            onPressed: () {
+              Navigator.pushNamed(context, "/cart");
+            },
+            child: Icon(CupertinoIcons.cart).iconColor(Colors.black),
+          ).badge(
+              color: Vx.red500,
+              size: 20,
+              count: _cart.items.length,
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              )),
+        ),
+
+        // appBar: AppBar(
+        //     centerTitle: true,
+        //     title:
+        //         Text("MY FLUTTER APP", style: TextStyle(color: Colors.black))),
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Tesco App".text.xl5.bold.color(Colors.indigo[900]).make(),
+                "On Sell ".text.xl2.color(Colors.indigo[900]).make(),
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                  Cataloglist().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            ),
+          ),
+        )
+        // Padding(
+        //   padding: const EdgeInsets.all(18.0),
+        //   child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+        //       ? GridView.builder(
+        //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //               mainAxisSpacing: 16, crossAxisCount: 3),
+        //           itemCount: CatalogModel.items.length,
+        //           itemBuilder: (context, index) {
+        //             return Padding(
+        //               padding: const EdgeInsets.all(8.0),
+        //               child: ItemWidget(
+        //                 item: CatalogModel.items[index],
+        //               ),
+        //             );
+        //           })
+        //       : Center(child: CircularProgressIndicator()),
+        //   // child: data != null
+        //   //     ? ListView.builder(
+        //   //         itemBuilder: (context, index) {
+        //   //           return Padding(
+        //   //             padding: const EdgeInsets.all(8.0),
+        //   //             child: ListTile(
+        //   //               leading: Icon(Icons.edit),
+        //   //               // leading: Image.network(data[index]["thumbnailUrl"]),
+        //   //               title: Text(data[index]["title"]),
+        //   //               subtitle: Text("ID ${data[index]["id"]}"),
+        //   //               tileColor: Colors.lime,
+        //   //               contentPadding: EdgeInsets.all(20),
+        //   //             ),
+        //   //           );
+        //   //         },
+        //   //         itemCount: data.length,
+        //   //       )
+        //   //     : Center(child: CircularProgressIndicator()),
+        // ),
+        // drawer: Drawer(
+        //   child: ListView(
+        //     children: [
+        //       // DrawerHeader(
+        //       //     child: Text("My Drawer", style: TextStyle(color: Colors.white)),
+        //       //     decoration: BoxDecoration(color: Colors.purple))
+        //       UserAccountsDrawerHeader(
+        //         accountName: Text("Wahaj Rashid"),
+        //         accountEmail: Text("wahaj1020@gmails.com"),
+        //         // currentAccountPicture: Image.network(src),
+        //       ),
+        //       ListTile(
+        //         leading: Icon(Icons.person),
+        //         title: Text("Accont"),
+        //         subtitle: Text("Personal Data"),
+        //         trailing: Icon(Icons.edit),
+        //       ),
+        //       ListTile(
+        //         leading: Icon(Icons.email),
+        //         title: Text("Email"),
+        //         subtitle: Text("Email Settings"),
+        //         trailing: Icon(Icons.edit),
+        //       ),
+        //       ListTile(
+        //         leading: Icon(Icons.info_rounded),
+        //         title: Text("Info"),
+        //         subtitle: Text("Personal Information"),
+        //         trailing: Icon(Icons.edit),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        // floatingActionButton: FloatingActionButton(
+        //     onPressed: () {
+        //       mytext = _nameController.text;
+        //       setState(() {});
+        //     },
+        //     child: Icon(Icons.refresh)),
+        // bottomSheet: FloatingActionButton(
+        //     onPressed: () {
+        //       Navigator.pushNamed(context, "/login");
+        //     },
+        //     child: Icon(Icons.logout_rounded))
+
+        );
+  }
+}
+
+class Cataloglist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (context, index) {
+          final catalog = CatalogModel.items[index];
+          return InkWell(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DetailsPage(catalog: catalog)),
+            ),
+            child: Catalogitems(catalog: catalog),
+          );
+        });
+  }
+}
+
+class Catalogitems extends StatelessWidget {
+  final Item catalog;
+
+  const Catalogitems({Key key, @required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        Hero(
+            tag: Key(catalog.id.toString()),
+            child: Catalogimage(image: catalog.image)),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            catalog.name.text.xl.color(Colors.white).make(),
+            // catalog.desc.text
+            //     .color(Colors.white)
+            //     .textStyle(context.captionStyle)
+            //     .make(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  "\$${catalog.price}"
+                      .text
+                      .color(Colors.lightGreenAccent[400])
+                      .bold
+                      .xl
+                      .make(),
+                  addtocart(catalog: catalog)
+                ],
+              ),
+            )
+          ],
+        ))
+      ],
+    )).color(Colors.indigo[900]).rounded.square(150).make().py16();
+  }
+}
+
+// ignore: camel_case_types
+// ignore: camel_case_types
+
+class Catalogimage extends StatelessWidget {
+  final String image;
+
+  const Catalogimage({Key key, @required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .roundedSM
+        .shadowOutline(outlineColor: Colors.black)
+        .color(Colors.white)
+        .make()
+        .w24(context)
+        .h20(context);
+  }
+}
